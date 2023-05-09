0277a66ef4dfe3f8a9530d5877e346297f193382
Nicolas <nicolascarobene1@gmail.com>
2023-05-09 13:45:42 +0200

    add .env at gitignore and process.env file UseStractor

--- Middleware/UseStractor.ts
+++ Middleware/UseStractor.ts
@@ -1,5 +1,7 @@
 import jwt from 'jsonwebtoken'
-import { SECRET_WORD } from '../src/mongoConect'
+import dotenv from 'dotenv'
+dotenv.config()
+
 interface Jwt {
   username: string,
   id: string,
@@ -12,7 +14,7 @@ export const UseStractor = (req: any, resp: any, next: any) => {
   if (authorization?.toLowerCase().startsWith('bearer')) {
     token = authorization.substring(7)
   }
-  const decodedToken: Jwt = jwt.verify(token, SECRET_WORD) as Jwt
+  const decodedToken: Jwt = jwt.verify(token, process.env.SECRET_WORD as string) as Jwt
 
   if (!token || !decodedToken) {
     return resp.status(401).json({ error: 'somesing' })
--- src/mongo.ts
+++ src/mongo.ts
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
-import { MONGO_CONECT } from './mongoConect'
 
 mongoose.connect(process.env.MONGO_CONECT as string)
   .then(() => {
